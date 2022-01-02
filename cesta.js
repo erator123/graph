8b97c42e796018e86f8d9d43c39d4fd11123fc09
Michal Danihelka <54680713+erator123@users.noreply.github.com>
2022-01-02 14:17:59 +0100

    Add files via upload

---
+++ cesta.js
@@ -0,0 +1,61 @@
+let findShortestPath = (graph, startNode, endNode) => {
+ 
+    
+      let distances = {};
+    distances[endNode] = "Infinity";
+    distances = Object.assign(distances, graph[startNode]);
+    let parents = { endNode: null };
+    for (let child in graph[startNode]) {
+     parents[child] = startNode;
+    }
+     
+    
+      let visited = [];
+      let node = shortestDistanceNode(distances, visited);
+    
+    
+    while (node) {
+    
+     let distance = distances[node];
+     let children = graph[node]; 
+         
+    
+         for (let child in children) {
+     
+     
+           if (String(child) === String(startNode)) {
+             continue;
+          } else {
+             
+             let newdistance = distance + children[child];
+  
+             if (!distances[child] || distances[child] > newdistance) {
+ 
+        distances[child] = newdistance;
+
+        parents[child] = node;
+       } 
+            }
+          }  
+ 
+         visited.push(node);
+         node = shortestDistanceNode(distances, visited);
+       }
+     
+    
+    let shortestPath = [endNode];
+    let parent = parents[endNode];
+    while (parent) {
+     shortestPath.push(parent);
+     parent = parents[parent];
+    }
+    shortestPath.reverse();
+     
+    
+    let results = {
+     distance: distances[endNode],
+     path: shortestPath,
+    };
+    
+      return results;
+   };
